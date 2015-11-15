d442b66279713aff1e9ee0506507fd1b94bb5217
castellanos70 <castellanos70@gmail.com>
2015-11-14 20:19:17 -0700

    added server package folder

---
+++ src/starvationevasion/server/Server.java
@@ -0,0 +1,12 @@
+package starvationevasion.server;
+
+/**
+ * Entry point for Server
+ */
+public class Server
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hi. I am the Server");
+  }
+}

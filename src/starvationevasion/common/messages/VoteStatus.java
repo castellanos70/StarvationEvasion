a043487f41ca56fc3256cf148c7b44a1953d2945
Shea <env.laser@gmail.com>
2015-12-02 21:01:49 -0700

    Made VoteStatus serializable

--- src/starvationevasion/common/messages/VoteStatus.java
+++ src/starvationevasion/common/messages/VoteStatus.java
@@ -3,12 +3,14 @@ package starvationevasion.common.messages;
 import starvationevasion.common.EnumRegion;
 import starvationevasion.common.PolicyCard;
 
+import java.io.Serializable;
+
 /**
  * Shea Polansky
  * A message from server to client informing them of the current
  * vote tallies.
  */
-public class VoteStatus
+public class VoteStatus implements Serializable
 {
   public final PolicyCard[] currentCards;
 

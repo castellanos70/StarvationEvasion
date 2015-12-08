9beb58e0f6db1bc7ba229875ddea13d2c0156f32
Shea Polansky <shea@polansky.co>
2015-12-07 21:58:11 -0700

    Print usage information for server

--- src/starvationevasion/server/Server.java
+++ src/starvationevasion/server/Server.java
@@ -66,6 +66,11 @@ public class Server
   */
   public static void main(String[] args)
   {
+    if (args.length < 2)
+    {
+      System.out.println("Usage: java -jar Server.jar /path/to/password/file.tsv command to launch ai");
+      return;
+    }
     new Server(args[0], Arrays.copyOfRange(args, 1, args.length)).start();
   }
 

b8abe01fb322cf9226b36f434889fbfbc18fa8c4
Alex Baker <alexebaker@alexebaker.com>
2015-12-07 21:33:34 -0700

    added region center

--- src/starvationevasion/sim/events/Drought.java
+++ src/starvationevasion/sim/events/Drought.java
@@ -3,7 +3,6 @@ package starvationevasion.sim.events;
 
 import starvationevasion.common.*;
 import starvationevasion.sim.*;
-import starvationevasion.sim.events.AbstractEvent;
 
 
 /**
@@ -25,17 +24,11 @@ public class Drought extends AbstractEvent<Region>
         tile.setRainfall(tile.getRainfall() / 3);
       }
     }
-
     super.applyEffects();
   }
 
   public MapPoint getLocation()
   {
-    // This should be made more accurate
-    for (Territory t : landArea.getTerritories())
-    {
-      return t.getCapitolLocation();
-    }
-    return new MapPoint(0, 0);
+    return landArea.getCenter();
   }
 }

b91116ba32359243b5c073c950dba7276a760f0b
Chris_Wu_CS351 <cwu@unm.edu>
2016-01-10 16:31:25 -0700

    Empty classes added to Rayquaza gui/model packages

---
+++ src/starvationevasion/client/Rayquaza/gui/GUI.java
@@ -0,0 +1,8 @@
+package starvationevasion.client.Rayquaza.gui;
+
+/**
+ * Created by c on 1/10/2016.
+ */
+public class GUI
+{
+}
---
+++ src/starvationevasion/client/Rayquaza/model/ModelController.java
@@ -0,0 +1,8 @@
+package starvationevasion.client.Rayquaza.model;
+
+/**
+ * Created by c on 1/10/2016.
+ */
+public class ModelController
+{
+}

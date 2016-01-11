8246092b24c06be33e933365a35992d5f0c82a4e
Chris_Wu_CS351 <cwu@unm.edu>
2016-01-10 18:29:50 -0700

    Took GameOptions from MegaMawile and added it to Rayquaza.
    Empty methods in NetworkHandler

--- src/starvationevasion/client/Rayquaza/network/NetworkHandler.java
+++ src/starvationevasion/client/Rayquaza/network/NetworkHandler.java
@@ -1,5 +1,6 @@
 package starvationevasion.client.Rayquaza.network;
 
+import starvationevasion.client.MegaMawile.net.AbstractClient;
 import starvationevasion.client.Rayquaza.engine.Engine;
 
 /**
@@ -12,5 +13,43 @@ public class NetworkHandler
   public NetworkHandler(Engine engine)
   {
     this.engine = engine;
+  }
+
+  /**
+   * Connects the main client to a desired server.
+   * This needs to happen first because we need to know the status
+   * of the server, specifically how many people are on it! if there are
+   * already filled then we need to figure out how to handle AI.
+   */
+  public void createClient()
+  {
+  }
+
+  public AbstractClient getOurClient()
+  {
+    return null;
+  }
+
+  /**
+   * Connects the client to the server.
+   */
+  public void connectToServer()
+  {
+
+  }
+
+  /**
+   * Logs the player into the server.
+   */
+  public void loginToServer()
+  {
+
+  }
+
+  /**
+   * Disconnects a player from the server.
+   */
+  public void disconnectFromServer()
+  {
   }
 }

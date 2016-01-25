834277ea98cd44357de562e117fcebf3db84d32c
ChrisWuUNM <cwu@unm.edu>
2016-01-25 10:58:26 -0700

    added a comment to tileDataFormat.txt
    Added a MainClient_MegaMawile2.java to see the test fxml.
    
    Was messing with possible nested controllers. Going to be changed later due to design choices

---
+++ src/starvationevasion/client/MegaMawile2/MainClient_MegaMawile2.java
@@ -0,0 +1,27 @@
+package starvationevasion.client.MegaMawile2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The entry point
+ */
+public class MainClient_MegaMawile2 extends Application
+{
+  @Override
+  public void start(Stage primaryStage) throws Exception
+  {
+    Parent root = FXMLLoader.load(this.getClass().getResource("gui/fxml/Hand.fxml"));
+    primaryStage.setTitle("Starvation Evasion");
+    primaryStage.setScene(new Scene(root, 650, 500));
+    primaryStage.show();
+  }
+
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
+}
--- src/starvationevasion/client/MegaMawile2/controller/guiController/HandController.java
+++ src/starvationevasion/client/MegaMawile2/controller/guiController/HandController.java
@@ -1,21 +1,29 @@
 package starvationevasion.client.MegaMawile2.controller.guiController;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.TextFlow;
 import starvationevasion.client.MegaMawile2.controller.GameEngine;
+import starvationevasion.client.Rayquaza.model.simulationData.ComboBoxItem;
 import starvationevasion.common.EnumPolicy;
+import starvationevasion.common.EnumRegion;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 /**
  * Created by c on 1/17/2016.
  */
-public class HandController
+public class HandController implements Initializable
 {
   @FXML
   public ComboBox regionsComboBox;
@@ -24,7 +32,7 @@ public class HandController
   @FXML
   public TextField cropField1, cropField2, cropField3, cropField4, cropField5, cropField6, cropField7, cropField8, cropField9, cropField10, cropField11, cropField12;
   @FXML
-  public TextArea chatBox;
+  public TextFlow chatBox;
   @FXML
   public TextField chatField;
   @FXML
@@ -35,12 +43,6 @@ public class HandController
   private Map<ImageView, EnumPolicy> handMap;
 
   private EnumPolicy selectedCard;
-
-  HandController(GameEngine gameEngine)
-  {
-    this.gameEngine = gameEngine;
-    initHandMap();
-  }
 
   private void initHandMap()
   {
@@ -54,6 +56,21 @@ public class HandController
     handMap.put(cardSeven, null);
   }
 
+  private void initRegions()
+  {
+    ObservableList<String> regions =
+      FXCollections.observableArrayList(
+        "California",
+        "Heartland",
+        "Northern Plains",
+        "Southeast",
+        "Northern Crescent",
+        "Southern Plains",
+        "Mountain"
+      );
+    regionsComboBox.setItems(regions);
+  }
+
   public void cardClicked(Event event)
   {
     ImageView card = (ImageView) event.getSource();
@@ -63,5 +80,12 @@ public class HandController
   public EnumPolicy getSelectedCard()
   {
     return selectedCard;
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources)
+  {
+    initHandMap();
+    initRegions();
   }
 }

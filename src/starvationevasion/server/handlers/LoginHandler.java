9382494cf178b5c26d932b260c77ce623b0ed664
Javier C <youracow@gmail.com>
2016-02-19 18:11:00 -0700

    Impl. method stub for login

--- src/starvationevasion/server/handlers/LoginHandler.java
+++ src/starvationevasion/server/handlers/LoginHandler.java
@@ -3,6 +3,10 @@ package starvationevasion.server.handlers;
 
 import starvationevasion.server.Worker;
 import starvationevasion.server.Server;
+import starvationevasion.server.model.Endpoint;
+import starvationevasion.server.model.Request;
+import starvationevasion.server.model.Response;
+import starvationevasion.server.model.User;
 
 public class LoginHandler extends AbstractHandler
 {
@@ -14,7 +18,29 @@ public class LoginHandler extends AbstractHandler
   @Override
   protected boolean handleRequestImpl (Request request)
   {
+    if (request.getDestination() == Endpoint.LOGIN)
+    {
+      boolean auth = authenticate(request.getData()[0], request.getData()[1]);
+      if (auth)
+      {
+        m_response = new Response(server.timeDiff(), "SUCCESS");
+      }
+      else
+      {
+        m_response = new Response(server.timeDiff(), "FAIL");
+      }
+      getClient().send(m_response.toString());
+
+      return true;
+    }
 
     return false;
+  }
+
+  private boolean authenticate(String username, String password)
+  {
+    User s = server.getUserByUsername(username);
+    return s != null && s.getPassword().equals(password);
+
   }
 }

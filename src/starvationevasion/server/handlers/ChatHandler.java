9f2dcdf9fbeda6b31662b9c90ab37fa1dd13e349
Javier C <youracow@gmail.com>
2016-02-24 08:36:21 -0700

    added notes about next steps for impl.

--- src/starvationevasion/server/handlers/ChatHandler.java
+++ src/starvationevasion/server/handlers/ChatHandler.java
@@ -2,6 +2,7 @@ package starvationevasion.server.handlers;
 
 import starvationevasion.server.Worker;
 import starvationevasion.server.Server;
+import starvationevasion.server.model.Endpoint;
 import starvationevasion.server.model.Request;
 
 public class ChatHandler extends AbstractHandler
@@ -13,6 +14,20 @@ public class ChatHandler extends AbstractHandler
   @Override
   protected boolean handleRequestImpl (Request request)
   {
+    if (request.getDestination().equals(Endpoint.CHAT))
+    {
+
+      String destination = request.getData()[0];
+      String data = request.getData()[1];
+      String from = getClient().getUser().getRegion().name();
+
+      // Determine if the destination is by username or by region
+
+      // get the worker for that destination
+      // send data.
+
+      return true;
+    }
     return false;
   }
 }

20cc527779a148c9bb21e7cd744b43e24206d20a
Javier C <youracow@gmail.com>
2016-02-29 15:46:32 -0700

    added comments showing corresponding handler

--- src/starvationevasion/server/model/Endpoint.java
+++ src/starvationevasion/server/model/Endpoint.java
@@ -3,27 +3,33 @@ package starvationevasion.server.model;
 
 public enum Endpoint
 {
+  // UserHandler
   USER_CREATE("user_create"),
   USER_READ("user_read"),
   USER_UPDATE("user_update"),
   USERS("users"),
   USERS_LOGGED_IN("users_logged_in"),
   USERS_READY("users_ready"),
+
+  // LoginHandler
   LOGIN("login"),
 
 
   HAND_READ("hand_read"),
   HAND_UPDATE("hand_update"),
 
+  // VoteHandler
   VOTE_UP("vote_up"),
   VOTE_DOWN("vote_down"),
 
+  // CardHandler
   DRAFT_CARD("draft_card"),
   DRAW_CARD("draw_card"),
   DELETE_CARD("delete_card"),
 
   //draft can you draft, have you drafted you can onyl draft one voting card.
 
+  // ChatHandler
   CHAT("chat"),
   CARD("card"),
   

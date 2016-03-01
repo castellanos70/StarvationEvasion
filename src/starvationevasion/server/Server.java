adae6d6bf25a45a6901a7e8840a5fe122fd28279
Javier C <youracow@gmail.com>
2016-03-01 02:20:07 -0700

    line ending adding even more

--- src/starvationevasion/server/Server.java
+++ src/starvationevasion/server/Server.java
@@ -383,10 +383,10 @@ public class Server
         else
         {
           // System.out.println(reading);
-          worker.send("HTTP/1.1 101 Switching Protocols\n" +
-                      "Upgrade: websocket\n" +
-                      "Connection: Upgrade\n" +
-                      "Sec-WebSocket-Accept: " + socketKey + "\n\r");
+          worker.send("HTTP/1.1 101 Switching Protocols\r\n" +
+                      "Upgrade: websocket\r\n" +
+                      "Connection: Upgrade\r\n" +
+                      "Sec-WebSocket-Accept: " + socketKey + "\r\n" + "\r\n");
         
           return true;
         }

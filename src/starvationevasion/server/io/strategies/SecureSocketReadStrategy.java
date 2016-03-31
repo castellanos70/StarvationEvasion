0fbb07f65050810bf95c51ffda931b3c668e5cf0
Javier C <youracow@gmail.com>
2016-03-31 03:52:00 -0600

    Idea of secure sockets

---
+++ src/starvationevasion/server/io/strategies/SecureSocketReadStrategy.java
@@ -0,0 +1,48 @@
+package starvationevasion.server.io.strategies;
+
+
+import starvationevasion.server.io.strategies.AbstractReadStrategy;
+import starvationevasion.server.model.Encryptable;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SecureSocketReadStrategy extends AbstractReadStrategy<String> implements Encryptable
+{
+  private String key;
+
+  public SecureSocketReadStrategy (Socket socket)
+  {
+    super(socket);
+  }
+
+  public SecureSocketReadStrategy (Socket socket, DataInputStream stream)
+  {
+    super(socket, stream);
+  }
+
+
+  @Override
+  public String read () throws IOException
+  {
+    return getStream().readUTF();
+  }
+
+  @Override
+  public void close () throws IOException
+  {
+    // socket.shutdownInput();
+  }
+
+  @Override
+  public void encrypt (String msg, String key)
+  {
+
+  }
+
+  @Override
+  public <T> T decrypt (String msg, String key)
+  {
+    return null;
+  }
+}
---
+++ src/starvationevasion/server/io/strategies/SecureSocketWriteStrategy.java
@@ -0,0 +1,51 @@
+package starvationevasion.server.io.strategies;
+
+
+import starvationevasion.server.io.strategies.AbstractWriteStrategy;
+import starvationevasion.server.model.Encryptable;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SecureSocketWriteStrategy extends AbstractWriteStrategy<String> implements Encryptable
+{
+  private final String key;
+
+  public SecureSocketWriteStrategy (Socket socket)
+  {
+    super(socket);
+    key = "kd";
+  }
+
+  public SecureSocketWriteStrategy (Socket socket, DataOutputStream stream)
+  {
+    super(socket, stream);
+    key = "kd";
+  }
+
+
+  @Override
+  public void write (String s) throws IOException
+  {
+    getStream().writeUTF(s);
+  }
+
+  @Override
+  public void close () throws IOException
+  {
+    // socket.shutdownOutput();
+  }
+
+  @Override
+  public void encrypt (String msg, String key)
+  {
+
+  }
+
+  @Override
+  public <T> T decrypt (String msg, String key)
+  {
+    return null;
+  }
+}

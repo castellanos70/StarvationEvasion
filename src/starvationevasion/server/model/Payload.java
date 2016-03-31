cc5151c839dd22b7446592528339d6e785c23565
Javier C <youracow@gmail.com>
2016-03-31 02:13:21 -0600

    implementing sendable... also added method bodies for impl of encryption

--- src/starvationevasion/server/model/Payload.java
+++ src/starvationevasion/server/model/Payload.java
@@ -1,10 +1,65 @@
 package starvationevasion.server.model;
 
+import com.oracle.javafx.jmx.json.JSONDocument;
+
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public interface Payload<K, V>
+public class Payload<K, V> extends HashMap<K, V> implements Sendable
 {
-  HashMap<K, V> getData();
 
-  void setData(HashMap<K, V> data);
+  @Override
+  public V put (K key, V value)
+  {
+    return super.put(key, value);
+  }
+
+  @Override
+  public V get (Object key)
+  {
+    return super.get(key);
+  }
+
+  @Override
+  public void putAll (Map<? extends K, ? extends V> m)
+  {
+    Iterator<? extends K> iterator = m.keySet().iterator();
+    while(iterator.hasNext())
+    {
+      K key = iterator.next();
+      put(key, m.get(key));
+    }
+  }
+
+  @Override
+  public V putIfAbsent (K key, V value)
+  {
+    // not supported
+    return super.putIfAbsent(key, value);
+  }
+
+  @Override
+  public void setType (String type)
+  {
+
+  }
+
+  @Override
+  public String getType ()
+  {
+    return (String) get("type");
+  }
+
+  @Override
+  public String toJSONString ()
+  {
+    return null;
+  }
+
+  @Override
+  public JSONDocument toJSON ()
+  {
+    return null;
+  }
 }

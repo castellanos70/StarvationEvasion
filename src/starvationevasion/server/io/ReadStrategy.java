74a05d26a3711b8d10665e30bd0e293bf5b38f1c
Javier C <youracow@gmail.com>
2016-03-31 02:35:32 -0600

    added accessors for stream

--- src/starvationevasion/server/io/ReadStrategy.java
+++ src/starvationevasion/server/io/ReadStrategy.java
@@ -1,10 +1,15 @@
 package starvationevasion.server.io;
 
 
+import java.io.DataInputStream;
 import java.io.IOException;
 
 public interface ReadStrategy<Result>
 {
   Result read () throws IOException, Exception;
   void close () throws IOException;
+
+  DataInputStream getStream ();
+
+  void setStream (DataInputStream inStream);
 }

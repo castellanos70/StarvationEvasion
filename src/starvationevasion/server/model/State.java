671557ebc7c24614a70998e6e9f7e1edfc703c81
Javier C <youracow@gmail.com>
2016-04-02 02:09:34 -0600

    state is now sendable

--- src/starvationevasion/server/model/State.java
+++ src/starvationevasion/server/model/State.java
@@ -1,7 +1,33 @@
 package starvationevasion.server.model;
 
 
-public enum State
+import com.oracle.javafx.jmx.json.JSONDocument;
+
+public enum State implements Sendable
 {
-  LOGIN, BEGINNING, DRAWING, DRAFTING, VOTING, WIN, LOSE, END
+  LOGIN, BEGINNING, DRAWING, DRAFTING, VOTING, WIN, LOSE, END;
+
+  @Override
+  public JSONDocument toJSON ()
+  {
+    return null;
+  }
+
+  @Override
+  public void fromJSON (Object doc)
+  {
+
+  }
+
+  @Override
+  public String getType ()
+  {
+    return null;
+  }
+
+  @Override
+  public void setType (String type)
+  {
+
+  }
 }

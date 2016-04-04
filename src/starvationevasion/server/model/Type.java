e5aa262db309cc775c8b1463142dd2d27436d2c2
Javier C <youracow@gmail.com>
2016-04-03 18:24:58 -0600

    added a type class. Mainly for JSON and receiving data.

---
+++ src/starvationevasion/server/model/Type.java
@@ -0,0 +1,20 @@
+package starvationevasion.server.model;
+
+
+public enum Type
+{
+  CHAT,
+
+  AUTH,
+
+  GAME,
+
+  USER_DATA,
+
+  USER,
+
+  FOOD,
+
+  BROADCAST, POLICY, REGION, SPECIAL_EVENT, POLICY_CARD, REGION_DATA;
+
+}

e919e9006725c1b627e13c22fea953c3b68b856e
Javier C <youracow@gmail.com>
2016-04-04 14:47:08 -0600

    implemented more endpoints for getting user and user info

--- src/starvationevasion/server/handlers/UserHandler.java
+++ src/starvationevasion/server/handlers/UserHandler.java
@@ -4,7 +4,6 @@ package starvationevasion.server.handlers;
  * @author Javier Chavez (javierc@cs.unm.edu)
  */
 
-import starvationevasion.common.EnumRegion;
 import starvationevasion.server.*;
 import starvationevasion.server.model.*;
 
@@ -28,13 +27,13 @@ public class UserHandler extends AbstractHandler
       String uname = (String) request.getPayload().get("username");
       String pwd = (String) request.getPayload().get("password");
 
-      if (server.addUser(new User(uname, pwd, null, new ArrayList<>())))
+      if (server.createUser(new User(uname, pwd, null, new ArrayList<>())))
       {
         m_response = new Response(server.uptime(), "SUCCESS");
       }
       else
       {
-        m_response = new Response(server.uptime(), "Authentication failed.");
+        m_response = new Response(server.uptime(), "Could not create user");
       }
       getClient().send(m_response);
       return true;
@@ -51,13 +50,13 @@ public class UserHandler extends AbstractHandler
     else if (request.getDestination().equals(Endpoint.USERS_LOGGED_IN))
     {
       ArrayList list = new ArrayList<User>();
-      for (User user : server.getActiveUserList())
+      for (User user : server.getLoggedInUsers())
       {
         list.add(user);
       }
 
       Payload data = new Payload();
-      data.putData(server.getActiveUserList());
+      data.putData(server.getLoggedInUsers());
 
       m_response = new Response(server.uptime(), data);
       getClient().send(m_response);
@@ -69,6 +68,7 @@ public class UserHandler extends AbstractHandler
       if (_u != null && _u.getRegion() != null)
       {
         _u.setPlaying(true);
+        server.addPlayer(_u);
         m_response = new Response(server.uptime(), "SUCCESS");
         getClient().send(m_response);
       }
@@ -77,18 +77,22 @@ public class UserHandler extends AbstractHandler
     }
     else if (request.getDestination().equals(Endpoint.USER_READ))
     {
-      User u = null;
+      Payload data = new Payload();
+
       if (request.getPayload().containsKey("username"))
       {
-        u = server.getUserByUsername((String) request.getPayload().get("username"));
+        data.putData(server.getUserByUsername((String) request.getPayload().get("username")));
       }
       else if (request.getPayload().containsKey("region"))
       {
-        u = server.getWorkerByRegion((EnumRegion) request.getPayload().get("region")).getUser();
+        ArrayList<User> _users = new ArrayList<>();
+        for (User user : server.getUserList())
+        {
+          _users.add(user);
+        }
+        data.putData(_users);
       }
 
-      Payload data = new Payload();
-      data.putData(u);
 
       m_response = new Response(data);
       getClient().send(m_response);

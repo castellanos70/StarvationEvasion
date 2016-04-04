e5594f19c67ea69ce1c58f39a138065096b382ed
Javier C <youracow@gmail.com>
2016-04-03 20:46:45 -0600

    added type to tojson method

--- src/starvationevasion/server/model/State.java
+++ src/starvationevasion/server/model/State.java
@@ -8,13 +8,14 @@ import com.oracle.javafx.jmx.json.JSONDocument;
 
 public enum State implements Sendable
 {
-  LOGIN, BEGINNING, DRAWING, DRAFTING, VOTING, WIN, LOSE, END;
+  LOGIN, BEGINNING, DRAWING, DRAFTING, VOTING, WIN, LOSE, END, TRANSITION;
 
   @Override
   public JSONDocument toJSON ()
   {
     JSONDocument _json = JSONDocument.createObject();
     _json.setString("name", name());
+    _json.setString("type", getType().name());
     return _json;
   }
 

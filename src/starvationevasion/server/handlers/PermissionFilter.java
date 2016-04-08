cf46e1244dfee886e8124e63019456c73a6417c2
Javier C <youracow@gmail.com>
2016-04-07 22:12:56 -0600

    Added permission filter to chain of resp.

---
+++ src/starvationevasion/server/handlers/PermissionFilter.java
@@ -0,0 +1,21 @@
+package starvationevasion.server.handlers;
+
+
+import starvationevasion.server.Server;
+import starvationevasion.server.Worker;
+import starvationevasion.server.model.Request;
+
+public class PermissionFilter extends AbstractHandler
+{
+  public PermissionFilter (Server server, Worker client)
+  {
+    super(server, client);
+  }
+
+  @Override
+  protected boolean handleRequestImpl (Request request)
+  {
+    // No command will pass permission gate.
+    return getClient().getUsername().equals("ANON");
+  }
+}

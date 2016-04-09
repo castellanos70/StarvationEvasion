720d271b13dc20debd8ae191b6d26c4e879b6a2f
Javier C <youracow@gmail.com>
2016-04-09 16:20:31 -0600

    Added comments explaining class and why its there

--- src/starvationevasion/server/model/ResponseFactory.java
+++ src/starvationevasion/server/model/ResponseFactory.java
@@ -1,6 +1,15 @@
 package starvationevasion.server.model;
 
+/**
+ * @author Javier Chavez (javierc@cs.unm.edu)
+ */
 
+/**
+ * Class that creates responses.
+ *
+ * Keeps the code a little cleaner. Makes the Response creation centralized to prevent bugs
+ * especially since we have many different ways of serializing objects in this codebase.
+ */
 public class ResponseFactory
 {
   private static ResponseFactory ourInstance = new ResponseFactory();
@@ -15,26 +24,45 @@ public class ResponseFactory
   {
   }
 
+  /**
+   * Returns a Response
+   * @param time current server time
+   * @param data data to be added
+   * @param type type of response we are sending
+   *
+   * @return Response containing all the supplied data
+   */
   public static Response build (double time, Sendable data, Type type)
   {
-
+    // if the data is null create a new payload
     if (data == null)
     {
       return new Response(time, new Payload(), type);
     }
-
+    // if it's already a payload just call constructor with data
     if (data instanceof Payload)
     {
       return new Response(time, (Payload) data, type);
     }
     else
     {
+      // clear the static object
       payload.clear();
+      // add the data
       payload.putData(data);
       return new Response(time, payload, type);
     }
   }
 
+  /**
+   * Returns a Response
+   * @param time current server time
+   * @param data data to be added
+   * @param type type of response we are sending
+   * @param message string to be added to payload "message"
+   *
+   * @return Response containing all the supplied data
+   */
   public static Response build (double time, Sendable data, Type type, String message)
   {
     Response response = ResponseFactory.build(time, data, type);

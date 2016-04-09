556acabdd0a7c16814256b0097605a5b37789afa
Javier C <youracow@gmail.com>
2016-04-08 22:00:09 -0600

    added where to select and values to insert

--- src/starvationevasion/server/model/db/backends/Backend.java
+++ src/starvationevasion/server/model/db/backends/Backend.java
@@ -3,6 +3,7 @@ package starvationevasion.server.model.db.backends;
 
 import java.sql.ResultSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 public interface Backend
 {
@@ -10,9 +11,9 @@ public interface Backend
 
   void createTable (String table, Hashtable<String, Object> properties);
 
-  void insert (String table);
+  void insert (String table, Set<Object> values);
 
-  ResultSet select (String table);
+  ResultSet select (String table, String where);
 
   void close();
 }

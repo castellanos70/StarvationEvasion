0ec3ca8b143021e1008c515369387e75ffeadef8
Javier C <youracow@gmail.com>
2016-04-22 20:38:51 -0600

    updated header to reflect that HTTP is not done

--- src/starvationevasion/server/io/strategies/HTTPWriteStrategy.java
+++ src/starvationevasion/server/io/strategies/HTTPWriteStrategy.java
@@ -30,8 +30,7 @@ public class HTTPWriteStrategy extends AbstractWriteStrategy
     String _jsonString = s.toJSON().toJSON();
     StringBuilder _sb = new StringBuilder();
 
-    // Here I am going to assume if a client is using a standard socket they want JSON
-    _sb.append("HTTP/1.0 200 OK\r\n");
+    _sb.append("HTTP/1.0 501 Not Implemented\r\n");
     _sb.append("Date: ").append(Util.getServerTime()).append("\r\n");
     _sb.append("Content-Type: application/json\r\n");
     _sb.append("\r\n");

c0add4bf75ede9d1f0394b7b4c3aa3b42651d962
Javier C <youracow@gmail.com>
2016-04-24 17:44:59 -0600

    reverted back to old version

--- src/starvationevasion/common/Tuple.java
+++ src/starvationevasion/common/Tuple.java
@@ -1,14 +1,10 @@
 package starvationevasion.common;
 
-import com.oracle.javafx.jmx.json.JSONDocument;
-import starvationevasion.server.model.Sendable;
-import starvationevasion.server.model.Type;
-
 /**
  * Shea Polansky
  * Simple generic 2-Tuple class
  */
-public class Tuple<T, U> implements Sendable
+public class Tuple<T, U>
 {
   public final T a;
   public final U b;
@@ -19,21 +15,4 @@ public class Tuple<T, U> implements Sendable
     this.b = b;
   }
 
-  @Override
-  public Type getType ()
-  {
-    return Type.SUCCESS;
-  }
-
-  @Override
-  public JSONDocument toJSON ()
-  {
-    return null;
-  }
-
-  @Override
-  public void fromJSON (Object doc)
-  {
-
-  }
 }

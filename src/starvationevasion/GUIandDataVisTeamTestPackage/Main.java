c320a0e29ed5c68578811e12690cb3e947512fad
CS-Labs <cseely@unm.edu>
2016-04-26 01:50:18 -0600

    Adding a test package so we can test things. Also inside I added the data visulization popup I was working on for each region. Right now it is just a test but the way I set it up it should not be to hard to integrate it within the actual game.

---
+++ src/starvationevasion/GUIandDataVisTeamTestPackage/Main.java
@@ -0,0 +1,89 @@
+package GUIandDataVisTeamTestPackage;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+
+/**
+ * 
+ * @author Christian Seely
+ * 
+ * Test program to demonstrate a possible way of visualizing data
+ * for each region. 
+ * This is the main entry point and launches a picture of the map with
+ * a single regional icon. 
+ *
+ */
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {		
+		try {
+	    //The first screen will display the map. 
+		Parent root = FXMLLoader.load(getClass().getResource("/GUIandDataVisTeamTestPackage/Map.fxml"));	
+		String image = Main.class.getResource("/GUIandDataVisTeamTestPackage/map.png").toExternalForm();
+		root.setStyle("-fx-background-image: url('" + image + "'); " +
+		           "-fx-background-position: center center; " +
+		           "-fx-background-repeat: stretch;");
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

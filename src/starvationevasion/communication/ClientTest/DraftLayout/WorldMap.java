b4f920d8baab9b2e0fbe70c4ee7d689154523bd1
Stephano <amunra@unm.edu>
2016-04-27 03:16:50 -0600

    Updates ClientTest - Copies a huge amount of files over so they can be edited by us as we need to

---
+++ src/starvationevasion/communication/ClientTest/DraftLayout/WorldMap.java
@@ -0,0 +1,96 @@
+package starvationevasion.communication.ClientTest.DraftLayout;
+
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Scale;
+import starvationevasion.common.GeographicArea;
+import starvationevasion.communication.ClientTest.GUI;
+import starvationevasion.sim.Simulator;
+
+import java.util.ArrayList;
+
+//import java.awt.*;
+
+/**
+ * Created by Dayloki on 3/29/2016.
+ */
+public class WorldMap extends ScrollPane
+{
+  private GUI gui;
+  private double zoomLevel=1;
+  private StackPane zoomGroup = new StackPane();
+  private Canvas canvas;
+  Image imageMap;
+  public WorldMap(GUI gui)
+  {
+   // setHbarPolicy(ScrollBarPolicy.NEVER);
+   // setVbarPolicy(ScrollBarPolicy.NEVER);
+
+    this.gui=gui;
+    imageMap=gui.getImageGetter().getWorldMap();
+    ImageView worldMap=new ImageView(imageMap);
+
+    VBox holder=new VBox();
+    holder.getChildren().add(worldMap);
+//    worldMap.setFitWidth(gui.getBoxWidth() * 10);
+//    worldMap.setLayoutX(gui.getBoxWidth() * 10);
+    //worldMap.setFitHeight(gui.getBoxHeight()*6);
+    worldMap.setPreserveRatio(true);
+    worldMap.setSmooth(true);
+    worldMap.setCache(true);
+//    worldMap.fitHeightProperty().bind(gui.getPrimaryStage().heightProperty());
+//    worldMap.fitWidthProperty().bind(gui.getPrimaryStage().widthProperty());
+
+    setPannable(true);
+    zoomGroup = new StackPane();
+    zoomGroup.getChildren().add(worldMap);
+    setContent(zoomGroup);
+    canvas=new Canvas(imageMap.getWidth(),imageMap.getHeight());
+    zoomGroup.getChildren().add(canvas);
+    //drawOnMap();
+    //drawOutline();
+    addEventFilter(ScrollEvent.SCROLL, event -> {
+
+      //      DoubleProperty zoomProperty = new SimpleDoubleProperty(200);
+//      worldMap.setFitHeight(zoomProperty.get());
+//      worldMap.setFitWidth(zoomProperty.get());
+
+      Group contentGroup = new Group();
+
+      contentGroup.getChildren().add(zoomGroup);
+
+
+      setContent(contentGroup);
+      if(event.getDeltaY()>0&&zoomLevel<1.1)zoomLevel += .1;
+      else if(event.getDeltaY()<0 &&zoomLevel>.5) zoomLevel -= .1;
+      Scale scaleTransform = new Scale(zoomLevel, zoomLevel, 0,0);
+      zoomGroup.setScaleX(zoomLevel);
+      zoomGroup.setScaleY(zoomLevel);
+      // Scale scaleTransform = new Scale(2, 2, -100, -10000000);
+      // scaleTransform.transform(event.getX(),event.getY());
+       scaleTransform.setPivotX(-event.getX());
+      scaleTransform.setPivotY(-event.getY());
+
+      double oldHvalue=getHvalue();
+      double oldVvalue=getVvalue();
+      setHvalue(event.getX() /getWidth() + oldHvalue * zoomLevel);
+      setVvalue(event.getY()/ getHeight()+oldVvalue*zoomLevel);
+
+      //setHvalue(.75);
+     // setVvalue(.75);
+
+      //zoomGroup.getTransforms().removeAll();
+      //zoomGroup.getTransforms().add(scaleTransform);
+    });
+  }
+
+
+}
---
+++ src/starvationevasion/communication/ClientTest/SummaryBar.java
@@ -0,0 +1,176 @@
+package starvationevasion.communication.ClientTest;
+
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import starvationevasion.common.EnumRegion;
+
+import java.util.ArrayList;
+
+/**
+ * SummaryBar is the node which is displayed at the top of the GUI. It displays:
+ *
+ * Game's current year
+ * Population of world and player region
+ * HDI of world and player region
+ * Current balance of the player's farming sector government revenue
+ *
+ */
+public class SummaryBar extends GridPane
+{
+  private ArrayList<ColumnConstraints> colConstraintsList;
+  private ArrayList<RowConstraints> rowConstraintsList;
+
+  GUI gui;
+  private EnumRegion region;
+  static int year = 1981;
+
+  static int regionPopulationData;
+  static int worldPopulationData;
+
+  static int regionHDIdata;
+  static int worldHDIdata;
+
+  static int farmingBalance;
+
+  static Text regionTitle;
+  static Text yearLabel;
+  static Text balanceLabel;
+
+  static Text regionHDI;
+  static Text worldHDI;
+
+  static Text regionPopulation;
+  static Text worldPopulation;
+
+
+  /**
+   * Constructor for the SummaryBar
+   * @param gui which owns the SummaryBar
+   */
+  public SummaryBar(GUI gui)
+  {
+    this.gui = gui;
+    initializeGridSizes();
+    this.getColumnConstraints().addAll(colConstraintsList);
+    this.getRowConstraints().addAll(rowConstraintsList);
+
+    //set assignedRegion to whatever the GUI was passed
+    if (gui.assignedRegion == null)
+    {
+      region = EnumRegion.USA_CALIFORNIA;
+    }
+    else
+    {
+      region = gui.assignedRegion;
+    }
+
+    regionTitle = new Text();
+    regionTitle.setFont(Font.font(null, FontWeight.BOLD, 14));
+    regionTitle.setText(region.toString());
+
+    yearLabel = new Text("" + year);
+    yearLabel.setFont(Font.font(null, FontWeight.BOLD, 40));
+
+    regionPopulation = new Text("Region Population: " + regionPopulationData);
+    regionPopulation.setFont(Font.font(null, FontWeight.BOLD, 14));
+    worldPopulation = new Text("World Population: " + worldPopulationData);
+    worldPopulation.setFont(Font.font(null, FontWeight.BOLD, 14));
+
+    regionHDI = new Text("Region HDI: " + regionHDIdata);
+    regionHDI.setFont(Font.font(null, FontWeight.BOLD, 14));
+    worldHDI = new Text("World HDI: " + worldHDIdata);
+    worldHDI.setFont(Font.font(null, FontWeight.BOLD, 14));
+
+    balanceLabel = new Text("Balance: $" + farmingBalance + " million");
+    balanceLabel.setFont(Font.font(null, FontWeight.BOLD, 14));
+
+    this.add(regionTitle, 2, 0, 1, 1);
+
+   // this.setAlignment(Pos.CENTER);
+   // this.add(yearLabel, 0, 0, 1, 4);
+
+    //this.add(regionPopulation, 1, 1, 1, 1);
+    //this.add(worldPopulation, 1, 2, 1, 1);
+
+    //this.add(regionHDI, 3, 1, 1, 1);
+    //this.add(worldHDI, 3, 2, 1, 1);
+
+    //this.add(balanceLabel, 2,3,1,1);
+
+    this.getStylesheets().add("/starvationevasion/client/GUI/DraftLayout/style.css");
+    this.getStyleClass().add("summarybar");
+  }
+
+  /**
+   * Updates the summary bar information
+   * @param newYear new year to update to
+   * @param regPop new region population
+   * @param worldPop new world population
+   * @param regHDI new regionHDI
+   * @param wHDI new worldHDI
+   * @param newBalance new balance
+   */
+  public void updateSummarybar(int newYear, int regPop, int worldPop, int regHDI, int wHDI, int newBalance)
+  {
+    year = newYear;
+    regionPopulationData = regPop;
+    worldPopulationData = worldPop;
+    regionHDIdata = regHDI;
+    worldHDIdata = wHDI;
+    farmingBalance = newBalance;
+
+    yearLabel.setText("" + String.valueOf(year));
+    regionPopulation.setText("Region Population: " + regionPopulationData);
+    worldPopulation.setText("World Population: " + worldPopulationData);
+    regionHDI.setText("Region HDI: " + regionHDIdata);
+    worldHDI.setText("World HDI: " + worldHDIdata);
+    balanceLabel.setText("Balance: $" + farmingBalance + " million");
+
+    this.getChildren().removeAll();
+    getChildren().clear();
+    this.add(yearLabel, 0, 0, 1, 4);
+
+    this.add(regionPopulation, 1, 1, 1, 1);
+    this.add(worldPopulation, 1, 2, 1, 1);
+
+    this.add(regionHDI, 3, 1, 1, 1);
+    this.add(worldHDI, 3, 2, 1, 1);
+
+    this.add(balanceLabel, 2,3,1,1);
+
+  }
+  public void setRegion(EnumRegion region){this.region=region;}
+  /**
+   * Updates the balance on the summarybar to the new balance
+   * @param newBalance something
+   */
+  public void updateBalance(int newBalance)
+  {
+    farmingBalance = newBalance;
+    balanceLabel.setText("Balance: $" + farmingBalance + " million");
+  }
+
+  private void initializeGridSizes()
+  {
+    colConstraintsList = new ArrayList<>();
+    colConstraintsList.add(new ColumnConstraints());
+
+    for (int i = 0; i < 5; ++i)
+    {
+      colConstraintsList.add(new ColumnConstraints());
+      colConstraintsList.get(i).setPercentWidth(20);
+    }
+
+    rowConstraintsList = new ArrayList<>();
+    for (int i = 0; i < 4; ++i)
+    {
+      rowConstraintsList.add(new RowConstraints());
+      rowConstraintsList.get(i).setPercentHeight(25);
+    }
+  }
+
+}

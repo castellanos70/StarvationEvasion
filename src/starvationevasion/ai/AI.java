f27901cad30d191cae3985aaba38bb178248011f
jmccall2 <jmccall2@unm.edu>
2016-04-26 20:11:54 -0600

    Added aggregateData method.
    Added aggregateData method which is called before drafting is done to collect all relevant information about what happened in the world and region in the last turn. This information is stored in factorMap, It is used when evaluating what card the AI should draft.

--- src/starvationevasion/ai/AI.java
+++ src/starvationevasion/ai/AI.java
@@ -1,14 +1,27 @@
 package starvationevasion.ai;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
 import starvationevasion.ai.commands.*;
 import starvationevasion.common.Constant;
-import starvationevasion.common.PolicyCard;
 import starvationevasion.common.Util;
 import starvationevasion.common.WorldData;
+import starvationevasion.common.policies.PolicyCard;
 import starvationevasion.server.model.*;
 
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
@@ -17,9 +30,34 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
+
+import starvationevasion.ai.commands.Command;
+import starvationevasion.ai.commands.Draft;
+import starvationevasion.ai.commands.GameState;
+import starvationevasion.ai.commands.Login;
+import starvationevasion.ai.commands.Uptime;
+import starvationevasion.ai.commands.Vote;
+import starvationevasion.common.Constant;
+import starvationevasion.common.EnumFood;
+import starvationevasion.common.RegionData;
+import starvationevasion.common.SpecialEventData;
+import starvationevasion.common.Util;
+import starvationevasion.common.WorldData;
+import starvationevasion.server.model.Request;
+import starvationevasion.server.model.RequestFactory;
+import starvationevasion.server.model.Response;
+import starvationevasion.server.model.State;
+import starvationevasion.server.model.Type;
+import starvationevasion.server.model.User;
 
 public class AI
 {
@@ -33,7 +71,7 @@ public class AI
 
   private State state = null;
 
-  private ArrayList<WorldData> worldData;
+  public ArrayList<WorldData> worldData;
 
   private List<PolicyCard> ballot;
 
@@ -42,14 +80,39 @@ public class AI
 
   private StreamListener listener;
   private volatile boolean isRunning = true;
-
+  public int numTurns = 0;
   private Stack<Command> commands = new Stack<>();
+
+  /*
+   * Factors used as map keys to keep track of and store events that are
+   * happening in the world and in the region that this AI represents.
+   */
+  public enum WorldFactors
+  {
+    SEALEVEL, REVENUEBALANCE, POPULATION, UNDERNOURISHED, HDI, FOODPRODUCED, FOODINCOME, FOODIMPORTED, FOODEXPORTED, ETHANOLTAXCREDIT, FOODPRICE
+  }
+
+  // This map is used to store information about the world and region that will
+  // be used in selecting
+  // cards to play on each turn.
+  public Map<WorldFactors, ArrayList<Object[]>> factorMap = new EnumMap<WorldFactors, ArrayList<Object[]>>(
+      WorldFactors.class);
+
+  // The AI has a copy of the list of special events, if any occurred during the
+  // last turn.
+  public ArrayList<SpecialEventData> eventList = new ArrayList<>();
+
+  // List of pairs of cards played in previous hands.
+  public ArrayList<ArrayList<PolicyCard>> draftedCards = new ArrayList<>();
+
+  // The region that this AI represents.
+  String region = "";
 
   private SecretKey serverKey;
   private Cipher aesCipher;
   private KeyPair rsaKey;
 
-  private AI (String host, int portNumber)
+  private AI(String host, int portNumber)
   {
     setupSecurity();
     while (!openConnection(host, portNumber))
@@ -61,23 +124,113 @@ public class AI
     commands.add(new GameState(this));
     commands.add(new Uptime(this));
     commands.add(new Login(this));
-
-
     listenToUserRequests();
 
   }
 
-  private boolean openConnection (String host, int portNumber)
+  /**
+   * Jeffrey McCall Create the EnumMap that will be passed to Draft.java which
+   * will aid the AI in making decisions about which card to draft.
+   */
+  private void createEnumMap()
+  {
+    for (int i = 0; i < WorldFactors.values().length; i++)
+    {
+      factorMap.put(WorldFactors.values()[i], new ArrayList<Object[]>());
+    }
+  }
+
+  /**
+   * Jeffrey McCall Collect all of the important data about the world and region
+   * that has been updated since the last turn. Put this data in the factorMap
+   * which is used in decision making by the AI regarding which cards it wants
+   * to draft.
+   */
+  private void aggregateData()
+  {
+    region = u.region.name();
+    factorMap.clear();
+    createEnumMap();
+    for(int i=0;i<worldData.size();i++)
+    {
+      Double[] seaLevel =
+        { worldData.get(i).seaLevel };
+      factorMap.get(WorldFactors.SEALEVEL).add(seaLevel);
+      eventList.clear();
+      if (worldData.get(i).eventList.size() > 0)
+      {
+        for (SpecialEventData event : worldData.get(numTurns).eventList)
+        {
+          eventList.add(event);
+        }
+      }
+      RegionData thisRegion = null;
+      for (RegionData data : worldData.get(i).regionData)
+      {
+        if (data.region.name().equals(u.region.name()))
+        {
+          thisRegion = data;
+        }
+      }
+      Integer[] revenueBalance =
+      { thisRegion.revenueBalance };
+      factorMap.get(WorldFactors.REVENUEBALANCE).add(revenueBalance);
+      Integer[] population =
+      { thisRegion.population };
+      factorMap.get(WorldFactors.POPULATION).add(population);
+      Double[] undernourished =
+      { thisRegion.undernourished };
+      factorMap.get(WorldFactors.UNDERNOURISHED).add(undernourished);
+      Double[] hdi =
+      { thisRegion.humanDevelopmentIndex };
+      factorMap.get(WorldFactors.HDI).add(hdi);
+      Long[] foodProduced = new Long[EnumFood.SIZE];
+      for (int h = 0; h < EnumFood.SIZE; h++)
+      {
+        foodProduced[h] = thisRegion.foodProduced[h];
+      }
+      // Food produced over the last year in metric tons.
+      factorMap.get(WorldFactors.FOODPRODUCED).add(foodProduced);
+      Integer[] foodIncome = new Integer[EnumFood.SIZE];
+      for (int h = 0; h < EnumFood.SIZE; h++)
+      {
+        foodIncome[h] = thisRegion.foodIncome[h];
+      }
+      factorMap.get(WorldFactors.FOODINCOME).add(foodIncome);
+      Long[] foodImported = new Long[EnumFood.SIZE];
+      for (int h = 0; h < EnumFood.SIZE; h++)
+      {
+        foodImported[h] = thisRegion.foodImported[h];
+      }
+      factorMap.get(WorldFactors.FOODIMPORTED).add(foodImported);
+      Long[] foodExported = new Long[EnumFood.SIZE];
+      for (int h = 0; h < EnumFood.SIZE; h++)
+      {
+        foodExported[h] = thisRegion.foodExported[h];
+      }
+      factorMap.get(WorldFactors.FOODEXPORTED).add(foodExported);
+      Integer[] ethanolCredit =
+      { thisRegion.ethanolProducerTaxCredit };
+      factorMap.get(WorldFactors.ETHANOLTAXCREDIT).add(ethanolCredit);
+      Integer[] foodPrice = new Integer[EnumFood.SIZE];
+      for (int h = 0; h < EnumFood.SIZE; h++)
+      {
+        foodPrice[h] = worldData.get(i).foodPrice[h];
+      }
+      factorMap.get(WorldFactors.FOODPRICE).add(foodPrice);
+    }
+  }
+
+  private boolean openConnection(String host, int portNumber)
   {
 
     try
     {
       clientSocket = new Socket(host, portNumber);
-    }
-    catch(Exception e)
+    } catch (Exception e)
     {
       System.err.println("Client Error: Could not open connection to " + host
-                                 + " on port " + portNumber);
+          + " on port " + portNumber);
       e.printStackTrace();
       isRunning = false;
       return false;
@@ -86,34 +239,31 @@ public class AI
     try
     {
       writer = new DataOutputStream(clientSocket.getOutputStream());
-    }
-    catch(Exception e)
+    } catch (Exception e)
     {
       e.printStackTrace();
       return false;
     }
 
-
     try
     {
       reader = new DataInputStream(clientSocket.getInputStream());
-    }
-    catch(IOException e)
+    } catch (IOException e)
     {
       e.printStackTrace();
       return false;
     }
     isRunning = true;
 
-    Util.startServerHandshake(clientSocket, rsaKey, "JavaClient");
+    Util.startServerHandshake(clientSocket, rsaKey, DataType.POJO);
 
     return true;
 
   }
 
-  private void listenToUserRequests ()
+  private void listenToUserRequests()
   {
-    while(isRunning)
+    while (isRunning)
     {
       try
       {
@@ -135,45 +285,44 @@ public class AI
         }
         // wait a little
         Thread.sleep(1000);
-      }
-      catch(InterruptedException e)
+      } catch (InterruptedException e)
       {
         e.printStackTrace();
       }
     }
   }
 
-  public ArrayList<WorldData> getWorldData ()
+  public ArrayList<WorldData> getWorldData()
   {
     return worldData;
   }
 
-  public double getStartNanoSec ()
+  public double getStartNanoSec()
   {
     return startNanoSec;
   }
 
-  public State getState ()
+  public State getState()
   {
     return state;
   }
 
-  public User getUser ()
+  public User getUser()
   {
     return u;
   }
 
-  public Stack<Command> getCommands ()
+  public Stack<Command> getCommands()
   {
     return commands;
   }
 
-  public List<PolicyCard> getBallot ()
+  public List<PolicyCard> getBallot()
   {
     return ballot;
   }
 
-  public ArrayList<User> getUsers ()
+  public ArrayList<User> getUsers()
   {
     return users;
   }
@@ -181,22 +330,23 @@ public class AI
   /**
    * StreamListener
    *
-   * Handles reading stream from socket. The data is then outputted
-   * to the console for user.
+   * Handles reading stream from socket. The data is then outputted to the
+   * console for user.
    */
   private class StreamListener extends Thread
   {
 
-    public void run ()
+    public void run()
     {
       serverKey = Util.endServerHandshake(clientSocket, rsaKey);
-      while(isRunning)
+      while (isRunning)
       {
         read();
       }
     }
 
-    private void read ()
+    @SuppressWarnings("unchecked")
+    private void read()
     {
       try
       {
@@ -206,111 +356,99 @@ public class AI
         {
           u = (User) response.getPayload().getData();
 
-          send(new RequestFactory().chat(startNanoSec,
-                                   "ALL",
-                                   "Hi, I am " + u.getUsername() + ". I'll be playing using (crappy) AI.",
-                                   null));
+          send(new RequestFactory().chat(startNanoSec, "ALL", "Hi, I am "
+              + u.getUsername() + ". I'll be playing using (crappy) AI.",
+              null));
 
-          System.out.println("Hi, I am " + u.getUsername() + ". I'll be playing using (crappy) AI.");
-        }
-        else if (response.getType().equals(Type.AUTH_ERROR))
+          System.out.println("Hi, I am " + u.getUsername()
+              + ". I'll be playing using (crappy) AI.");
+        } else if (response.getType().equals(Type.AUTH_ERROR))
         {
           System.out.println("Error logging in");
           isRunning = false;
-        }
-        else if (response.getType().equals(Type.USER))
+        } else if (response.getType().equals(Type.USER))
         {
-          if (u != null && u.getUsername().equals(((User) response.getPayload().getData()).getUsername()))
+          if (u != null && u.getUsername()
+              .equals(((User) response.getPayload().getData()).getUsername()))
           {
             u = (User) response.getPayload().getData();
           }
 
-        }
-        else if (response.getType().equals(Type.TIME))
+        } else if (response.getType().equals(Type.TIME))
         {
           startNanoSec = (double) response.getPayload().get("data");
-        }
-        else if (response.getType().equals(Type.WORLD_DATA_LIST))
+        } else if (response.getType().equals(Type.WORLD_DATA_LIST))
         {
           // System.out.println("Getting a list of WorldData's");
           worldData = (ArrayList<WorldData>) response.getPayload().getData();
-        }
-        else if (response.getType().equals(Type.USERS_LOGGED_IN_LIST))
+        } else if (response.getType().equals(Type.USERS_LOGGED_IN_LIST))
         {
           // System.out.println("Getting a list of ready users");
           users = (ArrayList<User>) response.getPayload().getData();
-        }
-        else if (response.getType().equals(Type.WORLD_DATA))
+        } else if (response.getType().equals(Type.WORLD_DATA))
         {
-          // System.out.println("Getting a list of WorldData's");
           worldData.add((WorldData) response.getPayload().getData());
-        }
-        else if (response.getType().equals(Type.GAME_STATE))
+        } else if (response.getType().equals(Type.GAME_STATE))
         {
           // System.out.println("Getting state of server");
-          state = (starvationevasion.server.model.State) response.getPayload().getData();
+          state = (starvationevasion.server.model.State) response.getPayload()
+              .getData();
           if (state == starvationevasion.server.model.State.VOTING)
           {
             AI.this.commands.add(new Vote(AI.this));
-          }
-          else if (state == starvationevasion.server.model.State.DRAFTING)
+          } else if (state == starvationevasion.server.model.State.DRAFTING)
           {
-            AI.this.commands.add(new Draft(AI.this));
-          }
-          else if (state == starvationevasion.server.model.State.DRAWING)
+            aggregateData();
+            draftedCards.add(new ArrayList<PolicyCard>());
+            Draft newDraft = new Draft(AI.this,region);
+            AI.this.commands.add(newDraft);
+            numTurns++;
+          } else if (state == starvationevasion.server.model.State.DRAWING)
           {
             // AI.this.commands.add(new Draft(AI.this));
             commands.clear();
           }
-        }
-        else if (response.getType().equals(Type.DRAFTED) || response.getType().equals(Type.DRAFTED_INTO_VOTE))
+        } else if (response.getType().equals(Type.DRAFTED)
+            || response.getType().equals(Type.DRAFTED_INTO_VOTE))
         {
           String msg = response.getPayload().getMessage();
           if (msg == null)
           {
             // PolicyCard card = (PolicyCard) response.getPayload().getData();
             // getUser().getHand().remove(card.getCardType());
-          }
-          else
+          } else
           {
             System.out.println(msg);
           }
-        }
-        else if (response.getType().equals(Type.VOTE_BALLOT))
+        } else if (response.getType().equals(Type.VOTE_BALLOT))
         {
           ballot = (List<PolicyCard>) response.getPayload().getData();
         }
 
-      }
-      catch(EOFException e)
+      } catch (EOFException e)
       {
         isRunning = false;
         System.out.println("Lost server, press enter to shutdown.");
 
-      }
-      catch(SocketException e)
+      } catch (SocketException e)
       {
         isRunning = false;
         System.out.println("Lost server");
         System.out.println("Shutting down...");
 
-      }
-      catch(NoSuchAlgorithmException e)
+      } catch (NoSuchAlgorithmException e)
       {
         isRunning = false;
         System.out.println("Will not be able to decrypt");
         System.exit(1);
-      }
-      catch(IOException e)
+      } catch (IOException e)
       {
         isRunning = false;
         System.out.println("Error reading");
-      }
-      catch(InvalidKeyException e)
+      } catch (InvalidKeyException e)
       {
         System.out.println("Key is not valid");
-      }
-      catch(ClassNotFoundException e)
+      } catch (ClassNotFoundException e)
       {
         isRunning = false;
         System.out.println("Class not found!");
@@ -318,7 +456,10 @@ public class AI
     }
   }
 
-  private Response readObject () throws IOException, ClassNotFoundException, InvalidKeyException, NoSuchAlgorithmException
+ private Response readObject () throws IOException,
+                                        ClassNotFoundException,
+                                        InvalidKeyException,
+                                        NoSuchAlgorithmException
   {
     int ch1 = reader.read();
     int ch2 = reader.read();
@@ -344,7 +485,7 @@ public class AI
     return response;
   }
 
-  public void send (Request request)
+  public void send(Request request)
   {
     try
     {
@@ -355,30 +496,25 @@ public class AI
       oos.writeObject(sealedObject);
       oos.close();
 
-
       writer.flush();
       byte[] bytes = baos.toByteArray();
       writer.writeInt(bytes.length);
       writer.write(bytes);
       writer.flush();
       baos.close();
-    }
-    catch(IOException e)
+    } catch (IOException e)
     {
       e.printStackTrace();
-    }
-    catch(InvalidKeyException e)
+    } catch (InvalidKeyException e)
     {
       e.printStackTrace();
-    }
-    catch(IllegalBlockSizeException e)
+    } catch (IllegalBlockSizeException e)
     {
       e.printStackTrace();
     }
   }
 
-
-  public static void main (String[] args)
+  public static void main(String[] args)
   {
 
     String host = null;
@@ -392,15 +528,17 @@ public class AI
       {
         throw new Exception();
       }
-    }
+    } 
     catch(Exception e)
     {
+      System.out.println("Usage: hostname port");
       System.exit(0);
     }
     new AI(host, port);
 
   }
-  private void setupSecurity ()
+
+  private void setupSecurity()
   {
     try
     {
@@ -409,11 +547,9 @@ public class AI
       keyGen.initialize(1024);
       rsaKey = keyGen.generateKeyPair();
       aesCipher = Cipher.getInstance(Constant.DATA_ALGORITHM);
-    }
-    catch (Exception e)
+    } catch (Exception e)
     {
       e.printStackTrace();
     }
   }
 }
-

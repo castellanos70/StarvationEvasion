f819680d69581e373843786c9ab37e165383a52c
bkorten <bkorten@unm.edu>
2016-05-07 20:32:11 -0600

    Update EventDriver.java
    Added a simple constructor and the skelleton for randomInit events

--- src/starvationevasion/sim/events/EventDriver.java
+++ src/starvationevasion/sim/events/EventDriver.java
@@ -2,17 +2,66 @@ package starvationevasion.sim.events;
 
 import java.util.ArrayList;
 
+import starvationevasion.common.EnumRegion;
+import starvationevasion.common.SpecialEventData.EnumSpecialEvent;
+import starvationevasion.common.Util;
+import starvationevasion.sim.Model;
+import starvationevasion.sim.Region;
+import starvationevasion.sim.Territory;
+
    
 
 public class EventDriver 
 {
  private ArrayList<AbstractEvent> EventList = new ArrayList<>();	
-
- public EventDriver()
+ private Model model;
+ 
+ public EventDriver(Model model)
  {
-	 
+  this.model=model;	 
 	 
  }
+ 
+ private void initRandomEvents()
+ {
+	 float eventChoice;
+	 
+	 int regionChoice;
+	 int terriotyChoice;
+	 int event;
+	 
+	 
+	 Region tmpRegion; 
+	 Territory tmpTerrioty;
+	 EnumRegion tmpEnumRegion;
+	 EnumRegion[] regionEnums = EnumRegion.values();
+	 while((eventChoice=Util.rand.nextFloat()) < 0.8)
+	 {
+		
+		 regionChoice = Util.rand.nextInt(EnumRegion.SIZE);
+		 tmpEnumRegion = regionEnums[regionChoice];
+		 tmpRegion = model.getRegion(tmpEnumRegion);
+		 
+		 terriotyChoice = Util.rand.nextInt(tmpRegion.getTerritoryList().size());
+		 tmpTerrioty = tmpRegion.getTerritoryList().get(terriotyChoice);
+		 
+		 event = (int) (eventChoice * 10);
+		 switch(event)
+		 {
+		 	// determine what events to add to Eventslist
+		 }
+	 }
+  
+ }
+ 
+ 
+ 
+ 
+ private void addEvent(AbstractEvent event)
+ {
+	 EventList.add(event);
+ }
+ 
  
  public void applyEvents()
  {

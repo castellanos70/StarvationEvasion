251cc21910cfd90e7ddd2ca5933fa9a44e649da7
Tyalinwis <Tyalinwis@gmail.com>
2016-06-29 10:30:26 -0600

    Added Documentation folder for non-auto generated docs. Moved JavaDoc folder from doc to JavaDoc. Placed JavaDoc folder in .gitignore.

--- src/starvationevasion/common/TestCardViewer.java
+++ src/starvationevasion/common/TestCardViewer.java
@@ -5,21 +5,32 @@ import java.util.Random;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.VBox;
 import starvationevasion.client.GUI.DraftLayout.CardView;
 
 public class TestCardViewer extends Application
 {
   private Scene scene;
   private Random random = new Random();
-  
+
+
+
   @Override
   public void start(Stage stage) throws Exception
   {
+    MenuBar menuBar = new MenuBar();
+
+    Menu menuPolicyCards = new Menu("Policy Cards");
+    menuBar.getMenus().addAll(menuPolicyCards);
+
     int index = random.nextInt(EnumPolicy.values().length);
 
     CardView card = new CardView(EnumRegion.USA_CALIFORNIA, EnumPolicy.values()[index]);
     
     scene = new Scene(card.getCardView(), 370, 520);
+    //scene.getRoot().getChildren().addAll(menuBar);
     stage.setScene(scene);
     stage.show();
   }
